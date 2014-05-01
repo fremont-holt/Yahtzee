7f6ef2855ad122cc27fe809c11fc6ec489a50739
Tim Holt <timholt@Tims-MacBook-Air.local>
2014-04-30 21:37:30 -0400

    Initial Commit

---
+++ Yahtzee/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Yahtzee
+//
+//  Created by Tim Holt on 4/30/14.
+//  Copyright (c) 2014 Tim Holt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
